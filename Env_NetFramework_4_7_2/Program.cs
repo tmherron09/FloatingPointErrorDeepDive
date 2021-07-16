689c6745cc961c366570d70096f6b75c75d64f58
Timothy M Herron <tmherron09@gmail.com>
2021-07-16 17:24:09 -0500

    Add .Net Framework Project - Initial running showing floating point rounding errors found in Unity/Mono but not .Net Core/Standard.

---
+++ Env_NetFramework_4_7_2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Versioning;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Env_NetFramework_4_7_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Get the Target Framework
+            string envName = ((TargetFrameworkAttribute)Assembly.GetExecutingAssembly()
+                .GetCustomAttributes(typeof(System.Runtime.Versioning.TargetFrameworkAttribute), false)
+                .SingleOrDefault()).FrameworkName;
+
+            FPE.Environment = envName;
+
+            FPE.Run();
+
+            Console.WriteLine("Press any key...");
+            Console.ReadKey();
+        }
+    }
+}
